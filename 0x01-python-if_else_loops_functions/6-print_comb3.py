17086419aa8b41ef1493394b941e32771ff57461
youniss5 <ahmedhyouniss@gmail.com>
2023-09-05 12:52:11 -0700

    islower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i, 10):
-        if i < j:
-            print("{:d}{:d}".format(i, j),
-                    end="\n" if i == 8 and j == 9 else ", ")
+for x in range(10):
+    for y in range(x, 10):
+        if x < y:
+            print("{:d}{:d}".format(x, y),
+                    end="\n" if x == 8 and y == 9 else ", ")
 
