8989ca7b369fbb70b4f0d5785cae52be2f59905c
youniss5 <ahmedhyouniss@gmail.com>
2023-09-25 15:47:38 -0700

    safe integer error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    is_integer = True
+    try:
+        print('{:d}'.format(value))
+    except Exception as error:
+        print("Exception:", error, file=sys.stderr)
+        is_integer = False
+    return is_integer
