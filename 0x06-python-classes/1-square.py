fc0e1c2c9e0e259102f1e60efff0391f1811cdbb
youniss5 <ahmedhyouniss@gmail.com>
2023-09-26 18:08:29 -0700

    size of square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,13 +2,11 @@
 """Module of a square"""
 
 
-
 class Square:
     """Square class"""
 
     def __init__(self, size):
         """Init is a constructior.
-
 
         Args:
             size: the size of a square.
