ce0b841de3b013712b664fb00d27b25dbbc5d171
youniss5 <ahmedhyouniss@gmail.com>
2023-09-26 18:19:35 -0700

    size of square and error raise

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""Module of a square"""
 
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initializing a square,
+
+        Args: size of square
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
