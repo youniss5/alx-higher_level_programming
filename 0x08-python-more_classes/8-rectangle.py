7f001ad1d611a77c50a17a6c8729756fc5d10034
youniss5 <ahmedhyouniss@gmail.com>
2023-10-02 19:37:13 -0700

    bigger rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -87,7 +87,4 @@ class Rectangle:
                 raise TypeError("rect_2 must be an instance of Rectangle")
             if rect_2.area() > rect_1.area():
                 return rect_2
-            return rect_1
-
-
-                                                                                                                                                
+            return rect_1                                                                                                                    
