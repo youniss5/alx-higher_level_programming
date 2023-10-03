c1fad8845effbf0420f6d7464d506815234d7278
youniss5 <ahmedhyouniss@gmail.com>
2023-10-02 19:54:26 -0700

    new square

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,6 +81,7 @@ class Rectangle:
             if rect_2.area() > rect_1.area():
                 return rect_2
             return rect_1
+
     @classmethod
     def square(cls, size=0):
         """new square"""
