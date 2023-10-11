a8b0245e390e6595bfd81fb8ff8c042e00d5a5bb
youniss5 <ahmedhyouniss@gmail.com>
2023-10-10 18:47:15 -0700

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" student class"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """init class of student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """ returns a dict of student class"""
+        return self.__dict__
