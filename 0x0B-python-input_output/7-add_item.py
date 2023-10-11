182cfae0ce7aad9d894ac3b780ae6a72168d75de
youniss5 <ahmedhyouniss@gmail.com>
2023-10-10 18:35:19 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ and then save them to a file
 
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 argulist = list(sys.argv[1:])
