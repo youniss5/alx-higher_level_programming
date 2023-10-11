97937c423ba54a4c36b8847f2c584a1bd925cc08
youniss5 <ahmedhyouniss@gmail.com>
2023-10-10 17:29:25 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""read_file function"""
+"""
+read_file function
+"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it"""
-    with open(filename, "r", encoding='utf-8') as fl:
-            print(fl.read(), end="")
+    """""Function that reads a text file(UTF8) and prints it"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
