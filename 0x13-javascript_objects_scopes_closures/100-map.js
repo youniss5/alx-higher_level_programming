621700e1b5a6083512bdd3e6b2e24423bd777498
youniss5 <ahmedhyouniss@gmail.com>
2023-12-12 11:28:29 -0800

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map((item, ind) => item * ind));
+console.log(list.map((item, index) => item * index));
