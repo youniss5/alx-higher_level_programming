aab961b838b1a859a49a91561bde931b13a813c1
youniss5 <ahmedhyouniss@gmail.com>
2023-12-17 14:30:39 -0800

    my filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument."""
+""" displays all values in the table of hbtn_0e_0_usa
+where name matches the argument
+"""
 import MySQLdb
 import sys
 
