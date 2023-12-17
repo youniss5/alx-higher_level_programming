7f1225d5fc0714d7f43c6c7e914af4a7fe1d6f3d
youniss5 <ahmedhyouniss@gmail.com>
2023-12-17 15:24:51 -0800

    model state filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    Sess = sessionmaker(bind=engine)
-    sess = Session()
-    for instance in sess.query(State).filter(State.name.like('%a%')):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like('%a%')):
         print(instance.id, instance.name, sep=": ")
