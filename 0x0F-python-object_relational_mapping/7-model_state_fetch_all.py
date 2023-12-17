621ed13dff94f1f26e2404228a059df148ff3304
youniss5 <ahmedhyouniss@gmail.com>
2023-12-17 15:19:50 -0800

    fetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in sess.query(State).order_by(State.id):
+    for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = sess.query(State).first()
+    instance = session.query(State).first()
     if instance is None:
         print("Nothing")
     else:
