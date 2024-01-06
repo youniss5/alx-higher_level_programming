1133a7353fd362d1ba4ff4303cf71e455023f67f
youniss5 <ahmedhyouniss@gmail.com>
2024-01-05 16:47:45 -0800

    post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL , ( post it )
-curl "$1" -sX POST -d "test@gmail.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
