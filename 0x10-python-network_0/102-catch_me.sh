ee3344346d967ffcf74492c6578aad1a0187daa8
youniss5 <ahmedhyouniss@gmail.com>
2024-01-05 17:03:05 -0800

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #hat causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "user_id=98" 0.0.0.0:5000/catch_me 
+curl -sL -X PUT -H "Origin: X-School-User-Id" -d "user_id=98" 0.0.0.0:5000/catch_me 
