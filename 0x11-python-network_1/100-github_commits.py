a7eb64aa5c75200bdc284032244c6e46a2e75daf
youniss5 <ahmedhyouniss@gmail.com>
2024-01-07 17:17:05 -0800

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that prints the last 10 recent commits on github"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for x in range(10):
+            print("{}: {}".format(
+                commits[x].get("sha"),
+                commits[x].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
