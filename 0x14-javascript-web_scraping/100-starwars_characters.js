22062bed33fea30d085ab589845ffc4262590215
youniss5 <ahmedhyouniss@gmail.com>
2024-01-31 04:34:12 -0800

    starwars chars

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,14 +3,14 @@
 const id = process.argv[2];
 const req = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(id + url, function (res, error, body) {
+req.get(id + url, function (error, res, body) {
   if (error) {
     console.log(error);
  }
   const data = JSON.parse(body);
   const aa = data.characters;
   for (const x of aa) {
-    req.get(x, function (res, error, body1) {
+    req.get(x, function (error, res, body1) {
       if (error) {
         console.log(error);
       }
