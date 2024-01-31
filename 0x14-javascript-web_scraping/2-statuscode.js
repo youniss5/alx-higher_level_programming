8da70020be65b88b3646c44763b5eb25e3a497f7
youniss5 <ahmedhyouniss@gmail.com>
2024-01-31 04:16:21 -0800

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const request = require('request');
 const episodeNum = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
@@ -6,10 +7,12 @@ const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
+  }
+ else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
-  } else {
+  }
+ else {
     console.log('Error code: ' + response.statusCode);
   }
 });
