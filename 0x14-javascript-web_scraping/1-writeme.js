066441c312f6972e1a40130d327a0abaa9e4488e
youniss5 <ahmedhyouniss@gmail.com>
2024-01-31 04:12:42 -0800

    status code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-#script that display the status code of a GET request.
+/*script that display the status code of a GET request.*/
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
